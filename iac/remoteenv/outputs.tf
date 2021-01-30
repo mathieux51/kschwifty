453eb43fc9d8735d92bccd60a2348e92844caa9d
Mathieu <mathieux51@gmail.com>
2021-01-30 16:24:05 +0100

    Update aws_availability_zones output

--- iac/remoteenv/outputs.tf
+++ iac/remoteenv/outputs.tf
@@ -3,7 +3,7 @@ output "cluster_name" {
 }
 
 output "aws_availability_zones" {
-  value = data.aws_availability_zones.cluster
+  value = data.aws_availability_zones.cluster.names
 }
 output "kops_state_store" {
   value = "s3://${aws_s3_bucket.kops_state_store.id}"
