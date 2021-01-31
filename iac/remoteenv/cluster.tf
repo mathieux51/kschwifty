d4213e3d4ac8560a84fb378cec746154c606607f
Mathieu <mathieux51@gmail.com>
2021-01-31 14:19:54 +0100

    Comment certifacte manager validation and add vpc

--- iac/remoteenv/cluster.tf
+++ iac/remoteenv/cluster.tf
@@ -1,4 +1,4 @@
-data "aws_availability_zones" "cluster" {
+data "aws_availability_zones" "available" {
   state = "available"
 }
 
@@ -50,4 +50,57 @@ resource "aws_s3_bucket" "kops_state_store" {
   # versioning {
   #   enabled = true
   # }
+  tags = {
+    Environment = local.environment
+    Application = "kops"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.cluster_name
+  cidr = local.cidr
+
+  azs             = data.availability_zones.available
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  tags = {
+    Environment = "${local.environment}"
+    Application = "network"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+resource "aws_security_group" "k8s_api_http" {
+  name   = "${local.environment}_k8s_api_http"
+  vpc_id = "${module.vpc.vpc_id}"
+  tags   = "${local.tags}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["${local.ingress_ips}"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["${local.ingress_ips}"]
+  }
 }
--- iac/remoteenv/outputs.tf
+++ iac/remoteenv/outputs.tf
@@ -1,10 +1,54 @@
 output "cluster_name" {
-  value = "${var.stage}.${var.domain_name}"
+  value = local.cluster_name
 }
 
 output "availability_zones" {
-  value = join(",", data.aws_availability_zones.cluster.names)
+  value = join(",", data.aws_availability_zones.available.names)
 }
 output "kops_state_store" {
   value = "s3://${aws_s3_bucket.kops_state_store.id}"
+}
+
+output "aws_region" {
+  value = var.aws_region
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  value = ["${module.vpc.public_subnets}"]
+}
+
+output "public_route_table_ids" {
+  value = ["${module.vpc.public_route_table_ids}"]
+}
+
+output "private_subnet_ids" {
+  value = ["${module.vpc.private_subnets}"]
+}
+
+output "private_route_table_ids" {
+  value = ["${module.vpc.private_route_table_ids}"]
+}
+
+output "default_security_group_id" {
+  value = "${module.vpc.default_security_group_id}"
+}
+
+output "nat_gateway_ids" {
+  value = "${module.vpc.natgw_ids}"
+}
+
+output "kops_s3_bucket_name" {
+  value = "${aws_s3_bucket.kops_state.bucket}"
+}
+
+output "k8s_api_http_security_group_id" {
+  value = "${aws_security_group.k8s_api_http.id}"
 }
