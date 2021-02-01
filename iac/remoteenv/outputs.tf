193c3e57b17a411a910d400928a8a24f6f7c8792
Mathieu <mathieux51@gmail.com>
2021-02-01 08:30:28 +0100

    Fix outputs and template

--- iac/remoteenv/outputs.tf
+++ iac/remoteenv/outputs.tf
@@ -23,19 +23,19 @@ output "vpc_cidr_block" {
 }
 
 output "public_subnet_ids" {
-  value = [module.vpc.public_subnets]
+  value = module.vpc.public_subnets
 }
 
 output "public_route_table_ids" {
-  value = [module.vpc.public_route_table_ids]
+  value = module.vpc.public_route_table_ids
 }
 
 output "private_subnet_ids" {
-  value = [module.vpc.private_subnets]
+  value = module.vpc.private_subnets
 }
 
 output "private_route_table_ids" {
-  value = [module.vpc.private_route_table_ids]
+  value = module.vpc.private_route_table_ids
 }
 
 output "default_security_group_id" {
